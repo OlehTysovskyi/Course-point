188866166022ce5a3704bf229ad767bb6c98f514
OlehTysovskyi <oleg.tisovsky228@gmail.com>
2025-05-12 12:11:27 +0300

    Changed course creation page logic

--- server/src/models/RegistrationRequest.js
+++ server/src/models/RegistrationRequest.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const RequestSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },      // буде plain
+    password: { type: String, required: true },
     role: { type: String, enum: ['student', 'teacher'], required: true },
     status: { type: String, enum: ['pending', 'approved', 'rejected'], default: 'pending' },
     createdAt: { type: Date, default: Date.now }
